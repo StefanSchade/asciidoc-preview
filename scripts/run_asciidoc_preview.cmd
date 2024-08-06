bfd53aec381c38e5b8d13013bc57a9af869bae99
Stefan Schade <stefan.schade@msg.group>
2024-08-06 09:25:48 +0200

    fix some problems
    Closes #1

--- scripts/run_asciidoc_preview.cmd
+++ scripts/run_asciidoc_preview.cmd
@@ -8,8 +8,20 @@ if "%1"=="" (
 
 set DOCUMENT_DIR=%~1
 
+IF NOT EXIST "%DOCUMENT_DIR%" (
+    echo The specified path does not exist.
+    exit /b 1
+)
+
+REM Check if the path is absolute
+echo %DOCUMENT_DIR% | findstr /R "^[a-zA-Z]:\\" > nul
+if %ERRORLEVEL% neq 0 (
+    REM Convert relative path to absolute path
+    set DOCUMENT_DIR=%~dp0%DOCUMENT_DIR%
+)
+
 cd ..
 set IMAGE_NAME=dev-environment
 docker build -t %IMAGE_NAME% -f "docker/Dockerfile" .
-docker run -it --rm -v %DOCUMENT_DIR%:/workspace/data -v %cd%/src:/workspace/src -w /workspace dev-environment
+docker run -it --rm -v %DOCUMENT_DIR%:/workspace/output -w /workspace dev-environment
 cd scripts
